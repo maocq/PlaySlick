f69f7fa74ba5f96f0190856476f64f06891538e1
maocq <carmonaesc@gmail.com>
2016-09-26 22:37:03 -0500

    WS

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,12 +6,13 @@ import actions.LoggingAction
 import play.api._
 import dal.UserRepository
 import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.libs.ws.WSClient
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class HomeController @Inject()(User: UserRepository)(val messagesApi: MessagesApi) extends Controller with I18nSupport{
+class HomeController @Inject()(User: UserRepository)(ws: WSClient)(val messagesApi: MessagesApi) extends Controller with I18nSupport{
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
@@ -21,6 +22,13 @@ class HomeController @Inject()(User: UserRepository)(val messagesApi: MessagesAp
     User.list map { users =>
       val title = Messages("home.users")
       Ok(views.html.users(title, users))
+    }
+  }
+
+  def wsApi = Action.async {
+    val url = "https://api.github.com/users/maocq/repos"
+    ws.url(url).get().map { response =>
+      Ok(response.body)
     }
   }
 
