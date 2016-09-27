4b6bd1af68529e972fa1108e9bb65c9b58a19ec6
maocq <carmonaesc@gmail.com>
2016-09-26 22:18:38 -0500

    Templates

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,26 @@
 package controllers
 
 import javax.inject._
+
 import actions.LoggingAction
 import play.api._
+import dal.UserRepository
 import play.api.mvc._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject()(User: UserRepository) extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def template = Action.async {
+    User.list map { users =>
+      val title = "Usuarios"
+      Ok(views.html.users(title, users))
+    }
   }
 
   def action = LoggingAction {
