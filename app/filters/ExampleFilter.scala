2fc34b4489d24848edf35c99ce6bb4df00c73e41
maocq <carmonaesc@gmail.com>
2016-09-26 22:43:07 -0500

    Filter

--- app/filters/ExampleFilter.scala
+++ app/filters/ExampleFilter.scala
@@ -2,19 +2,10 @@ package filters
 
 import akka.stream.Materializer
 import javax.inject._
+import play.api.Logger
 import play.api.mvc._
 import scala.concurrent.{ExecutionContext, Future}
 
-/**
- * This is a simple filter that adds a header to all requests. It's
- * added to the application's list of filters by the
- * [[Filters]] class.
- *
- * @param mat This object is needed to handle streaming of requests
- * and responses.
- * @param exec This class is needed to execute code asynchronously.
- * It is used below by the `map` method.
- */
 @Singleton
 class ExampleFilter @Inject()(
     implicit override val mat: Materializer,
@@ -22,9 +13,9 @@ class ExampleFilter @Inject()(
 
   override def apply(nextFilter: RequestHeader => Future[Result])
            (requestHeader: RequestHeader): Future[Result] = {
-    // Run the next filter in the chain. This will call other filters
-    // and eventually call the action. Take the result and modify it
-    // by adding a new header.
+
+    Logger.info(s"\tFiltro $requestHeader")
+
     nextFilter(requestHeader).map { result =>
       result.withHeaders("X-ExampleFilter" -> "foo")
     }
